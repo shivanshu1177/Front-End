609d4cd779c9b065ac8636015d8347df6e44ad8c
shivanshu1177 <shivanshu1177@chitkara.edu.in>
2022-09-16 10:06:10 +0530

    class

---
+++ 16thSep/noOne.js
@@ -0,0 +1,18 @@
+
+// $('h1').css('color','red')
+
+// $('h1').css({
+//     color: 'green',
+//     border: '1px solid red'
+// })
+
+// console.log($('h1').text('hy!!'))
+
+// $('button').click(function(){
+//     console.log('clicked')      
+// })
+
+
+$('h1').click(function(){
+    $(this).css('color','red')
+})
